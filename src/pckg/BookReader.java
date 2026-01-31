9ba5fa048f7fa5e0d68743aed79292b55bad682d
jacob-eager <jakeeager.04@gmail.com>
2026-01-30 20:57:49 -0500

    Extract method on genBookFromFields

--- src/pckg/BookReader.java
+++ src/pckg/BookReader.java
@@ -44,31 +44,7 @@ public class BookReader {
 				String line = scnr.nextLine();
 				String[] fields = safeCSVSplit(line);
 				
-				Book newBook = new Book(
-					    Integer.parseInt(fields[0]), // book_id
-					    Integer.parseInt(fields[1]), // goodreads_book_id
-					    Integer.parseInt(fields[2]), // best_book_id
-					    Integer.parseInt(fields[3]), // work_id
-					    Integer.parseInt(fields[4]), // books_count
-					    fields[5], 					// isbn 
-					    (long)Double.parseDouble(fields[6]),                  // isbn13
-					    fields[7],                  // authors
-					    (int)Double.parseDouble(fields[8]),         // originalPublicationYear
-					    fields[9],                  // originalTitle
-					    fields[10],                 // title
-					    fields[11],                 // languageCode
-					    Double.parseDouble(fields[12]), // averageRating
-					    Integer.parseInt(fields[13]), // ratingsCount
-					    Integer.parseInt(fields[14]), // workRatingsCount
-					    Integer.parseInt(fields[15]), // workTextReviewsCount
-					    Integer.parseInt(fields[16]), // ratings1
-					    Integer.parseInt(fields[17]), // ratings2
-					    Integer.parseInt(fields[18]), // ratings3
-					    Integer.parseInt(fields[19]), // ratings4
-					    Integer.parseInt(fields[20]), // ratings5
-					    fields[21],                 // imageURL
-					    fields[22]                  // smallImageURL
-					);
+				Book newBook = genBookFromFields(fields);
 				
 				if (validateBook(fields)) {
 					list.add(newBook);
@@ -86,6 +62,34 @@ public class BookReader {
 		return list;
 	}
 	
+	private static Book genBookFromFields(String[] fields) {
+		return new Book(
+			    Integer.parseInt(fields[0]), // book_id
+			    Integer.parseInt(fields[1]), // goodreads_book_id
+			    Integer.parseInt(fields[2]), // best_book_id
+			    Integer.parseInt(fields[3]), // work_id
+			    Integer.parseInt(fields[4]), // books_count
+			    fields[5], 					// isbn 
+			    (long)Double.parseDouble(fields[6]),                  // isbn13
+			    fields[7],                  // authors
+			    (int)Double.parseDouble(fields[8]),         // originalPublicationYear
+			    fields[9],                  // originalTitle
+			    fields[10],                 // title
+			    fields[11],                 // languageCode
+			    Double.parseDouble(fields[12]), // averageRating
+			    Integer.parseInt(fields[13]), // ratingsCount
+			    Integer.parseInt(fields[14]), // workRatingsCount
+			    Integer.parseInt(fields[15]), // workTextReviewsCount
+			    Integer.parseInt(fields[16]), // ratings1
+			    Integer.parseInt(fields[17]), // ratings2
+			    Integer.parseInt(fields[18]), // ratings3
+			    Integer.parseInt(fields[19]), // ratings4
+			    Integer.parseInt(fields[20]), // ratings5
+			    fields[21],                 // imageURL
+			    fields[22]                  // smallImageURL
+			);
+	}
+
 	private static String[] safeCSVSplit(String s) {
 		
 		String[] array = new String[23];
